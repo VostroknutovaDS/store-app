295c3905506c8f1003d085a9efdf1f6d5a1dff8a
Sarapultseva, Daria S <Sarapultseva.DS@parma.ru>
2021-06-27 22:36:58 +0500

    feat: set routing to grid

--- store-app/src/app/app-routing.module.ts
+++ store-app/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoreGridComponent } from './components/store-grid/store-grid';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: StoreGridComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ store-app/src/app/components/store-grid/store-grid.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-grid',
+  templateUrl: './store-grid.html',
+})
+export class StoreGridComponent {}
